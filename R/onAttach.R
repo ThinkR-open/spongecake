4156014b2d17b1b4fce49cfb9c8c251272ef07f1
demo <demo@thinkr.fr>
2016-10-16 22:55:23 +0200

    startup message

--- R/onAttach.R
+++ R/onAttach.R
@@ -5,30 +5,9 @@
         packageStartupMessage('')
         packageStartupMessage(pdesc$Package, " ", pdesc$Version, " by Vincent Guyader")
         packageStartupMessage('->  For help type ?spongecake')
+        packageStartupMessage('be sure to have ffmpeg on your computer (http://ffmpeg.org)')
         packageStartupMessage('')
     }
-  # oopts = if (.Platform$OS.type == "windows" & .Platform$r_arch == "x64") {
-  #
-  #   # options(ffmpeg = "D:/ThinkR/spongecake/inst/ffmpeg-3.1.4-win64-static/bin/ffmpeg.exe")
-  #   options(ffmpeg = file.path( find.package("spongecake"), "ffmpeg-3.1.4-win64-static/bin/ffmpeg.exe" ))
-  #
-  #
-  #
-  #     } else{    options(ffmpeg = "ffmpeg")}
+
   options(ffmpeg = "ffmpeg")
 }
-
-# enleve les faux positifs du check
-# globalVariables(c("code", "type_code", "effect", ".",
-#                   "caract", "caract_1", "caract_2",
-#                   "nom", "ID_caract", "ID",
-#                   "Code.LPP",
-#                   "Libelle",
-#                   "nb",
-#                   "UCD.7",
-#                   "Libelle",
-#                   "nb",
-#                   "effect",
-#                   "Y",
-#                   "xml_code"
-#                   ))
